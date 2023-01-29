660f0a6e288401bcab883f49f4928c026d733437
jihyunna <skwlgus106@naver.com>
2023-01-29 17:01:56 +0900

    initial commit

---
+++ src/main/java/com/example/SEENEMA/SeeNemaApplication.java
@@ -0,0 +1,13 @@
+package com.example.SEENEMA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeeNemaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeeNemaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SEENEMA/SeeNemaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SEENEMA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeeNemaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
