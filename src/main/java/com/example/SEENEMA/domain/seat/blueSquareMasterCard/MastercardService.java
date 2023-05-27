c1aae41f95d00c59230123b585cae4c4aa8d8854
jiyoonK <wldbs068@gmail.com>
2023-05-28 05:04:47 +0900

    :sparkles: #58 블루스퀘어 마스터카드홀 완료

--- src/main/java/com/example/SEENEMA/domain/seat/blueSquareMasterCard/MastercardService.java
+++ src/main/java/com/example/SEENEMA/domain/seat/blueSquareMasterCard/MastercardService.java
@@ -1,13 +1,13 @@
-package com.example.SEENEMA.domain.seat.blueSquareShinhan;
+package com.example.SEENEMA.domain.seat.blueSquareMasterCard;
 
 import com.example.SEENEMA.domain.post.file.Image;
 import com.example.SEENEMA.domain.post.file.ImageRepository;
 import com.example.SEENEMA.domain.post.view.dto.ResponseMessage;
 import com.example.SEENEMA.domain.seat.SeatDto;
-import com.example.SEENEMA.domain.seat.blueSquareShinhan.domain.ShinhanPost;
-import com.example.SEENEMA.domain.seat.blueSquareShinhan.domain.ShinhanSeat;
-import com.example.SEENEMA.domain.seat.blueSquareShinhan.repository.ShinhanPostRepository;
-import com.example.SEENEMA.domain.seat.blueSquareShinhan.repository.ShinhanRepository;
+import com.example.SEENEMA.domain.seat.blueSquareMasterCard.domain.MastercardPost;
+import com.example.SEENEMA.domain.seat.blueSquareMasterCard.domain.MastercardSeat;
+import com.example.SEENEMA.domain.seat.blueSquareMasterCard.repository.MastercardPostRepository;
+import com.example.SEENEMA.domain.seat.blueSquareMasterCard.repository.MastercardRepository;
 import com.example.SEENEMA.domain.theater.domain.Theater;
 import com.example.SEENEMA.domain.theater.repository.TheaterRepository;
 import com.example.SEENEMA.domain.user.domain.User;
@@ -23,56 +23,58 @@ import java.util.stream.Collectors;
 @Service
 @Transactional
 @RequiredArgsConstructor
-public class ShinhanService {
-    private final ShinhanRepository shinhanRepository;
-    private final ShinhanPostRepository shinhanPostRepository;
-    private final UserRepository userRepository;
-    private final TheaterRepository theaterRepository;
-    private final ImageRepository imageRepository;
+public class MastercardService {
+    private final MastercardRepository seatRepo;
+    private final MastercardPostRepository postRepo;
+    private final UserRepository userRepo;
+    private final TheaterRepository theaterRepo;
+    private final ImageRepository imageRepo;
     public static String convertToSeatNumber(int x, int y, int z) {
         String column = String.valueOf(x);  // 열은 x 좌표 그대로 사용
         String number = String.valueOf(y);  // 번호는 y 좌표 그대로 사용
         String floor = String.valueOf(z); // 층은 z 좌표 그대로 사용
-
-        return floor + "층 " + column + "열 " + number + "번";
+        if(x==1){
+            return floor + "층 B구역 " + number + "번";
+        }
+        else {
+            return floor + "층 " + column + "열 " + number + "번";
+        }
     }
     public SeatDto.addResponse createSeatPost(Long userId, Long theaterId, Long seatId, SeatDto.addRequest request){
         User user = getUser(userId);
         Theater theater = getTheater(theaterId);
-        ShinhanSeat seat = getSeat(seatId).get();
+        MastercardSeat seat = getSeat(seatId);
         List<Image> images = getImage(request.getImage());
 
         request.setUser(user);
         request.setTheater(theater);
         request.setImage(images);
 
-        ShinhanPost view = request.toShinhanPostEntity();
-        view.setShinhanSeat(seat);
+        MastercardPost view = request.toMastercardPostEntity();
+        view.setMastercardSeat(seat);
 
         List<Image> persistedImages = new ArrayList<>();
-        for(Image image : images){
-            persistedImages.add(imageRepository.save(image));
-        }
+        for(Image image : images)
+            persistedImages.add(imageRepo.save(image));
         view.setImage(persistedImages);
-        return new SeatDto.addResponse(shinhanPostRepository.save(view));
+        return new SeatDto.addResponse(postRepo.save(view));
     }
     @Transactional(readOnly = true)
     public SeatDto.addResponse readSeatPost(Long theaterId, Long seatId, Long viewNo){
-        ShinhanPost view = shinhanPostRepository.findById(viewNo).get();
+        MastercardPost view = postRepo.findByViewNo(viewNo);
         // 이미지 컬렉션을 명시적으로 초기화
         Hibernate.initialize(view.getImage());
         return new SeatDto.addResponse(view);
     }
     @Transactional
     public String authUserForEdit(Long theaterId, Long seatId, Long viewNo, Long userId){
-        ShinhanPost shinhanPost = getSeatPost(viewNo);
-        if(shinhanPost.getUser().getUserId().equals(userId)) return "SUCCESS";
+        MastercardPost mastercardPost = getSeatPost(viewNo);
+        if(mastercardPost.getUser().getUserId().equals(userId)) return "SUCCESS";
         return "NOT_SAME_USER";
     }
-
     @Transactional
-    public SeatDto.addResponse updateSeatPost(Long theaterId, Long sesatId, Long viewNo, SeatDto.updateRequest request, Long userId){
-        ShinhanPost seatPost = getSeatPost(viewNo);
+    public SeatDto.addResponse updateSeatPost(Long theaterId, Long seatId, Long viewNo, SeatDto.updateRequest request, Long userId){
+        MastercardPost seatPost = getSeatPost(viewNo);
         // update 전 작성자와 사용자 동일 판별
         if(!seatPost.getUser().getUserId().equals(userId)) return new SeatDto.addResponse(seatPost);
         else{
@@ -85,19 +87,16 @@ public class ShinhanService {
     @Transactional
     public String deleteSeatPost(Long theaterId, Long seatId, Long viewNo, Long userId){
         // 삭제
-        ShinhanPost seatPost = getSeatPost(viewNo);
+        MastercardPost seatPost = getSeatPost(viewNo);
         if(seatPost.getUser().getUserId().equals(userId)){
-            shinhanPostRepository.delete(seatPost);
+            postRepo.delete(seatPost);
             return ResponseMessage.DELETE.getMsg();
         }
-        else {
-            return "FAIL";
-        }
+        else return "FAIL";
     }
 
-
     public SeatDto.postList getListBySeat(Long theaterId, Long seatId){
-        List<SeatDto.seatViewList> seatViewLists = shinhanPostRepository.findByTheater_TheaterIdAndShinhanSeat_SeatId(theaterId, seatId).stream()
+        List<SeatDto.seatViewList> seatViewLists = postRepo.findByTheater_TheaterIdAndMastercardSeat_SeatId(theaterId, seatId).stream()
                 .map(SeatDto.seatViewList::new)
                 .collect(Collectors.toList());
         Collections.sort(seatViewLists, Collections.reverseOrder());
@@ -110,50 +109,45 @@ public class ShinhanService {
         else{
             response.setPostedYN(Boolean.TRUE);
             int avr = 0;
-            for(SeatDto.seatViewList s : seatViewLists) {
+            for(SeatDto.seatViewList s : seatViewLists)
                 avr += s.getAverage();
-            }
             avr /= seatViewLists.size();
             response.setAverage(avr);
-            return response;
+            return  response;
         }
     }
-
     public List<SeatDto.seatViewList> getListByTheater(Long theaterId){
         // 공연장별 게시글 조회
-        List<SeatDto.seatViewList> response = shinhanPostRepository.findByTheater_TheaterId(theaterId).stream()
+        List<SeatDto.seatViewList> response = postRepo.findByTheater_TheaterId(theaterId).stream()
                 .map(SeatDto.seatViewList::new)
                 .collect(Collectors.toList());
         Collections.sort(response, Collections.reverseOrder());
         return response;
     }
 
-    public List<SeatDto.seatAverage> getAverageList(Long theaterId) {
+    public List<SeatDto.seatAverage> getAverageList(Long theaterId){
         // 공연장 전체 좌석 평균 목록
-        List<ShinhanPost> posts = shinhanPostRepository.findByTheater_TheaterId(theaterId);
-        Map<Long, List<ShinhanPost>> seatPostMap = new HashMap<>();
+        List<MastercardPost> posts = postRepo.findByTheater_TheaterId(theaterId);
+        Map<Long, List<MastercardPost>> seatPostMap = new HashMap<>();
         List<SeatDto.seatAverage> seatAverages = new ArrayList<>();
 
         // 좌석별 게시물 그룹화
-        for(ShinhanPost post : posts){
-            Long seatId = post.getShinhanSeat().getSeatId();
-            if(seatPostMap.containsKey(seatId)){
-                seatPostMap.get(seatId).add(post);
-            }
+        for(MastercardPost post : posts){
+            Long seatId = post.getMastercardSeat().getSeatId();
+            if(seatPostMap.containsKey(seatId)) seatPostMap.get(seatId).add(post);
             else{
-                List<ShinhanPost> postList = new ArrayList<>();
+                List<MastercardPost> postList = new ArrayList<>();
                 postList.add(post);
                 seatPostMap.put(seatId, postList);
             }
         }
-
         // 좌석별 평균 계산
-        for(Map.Entry<Long, List<ShinhanPost>> entry : seatPostMap.entrySet()){
+        for(Map.Entry<Long, List<MastercardPost>> entry : seatPostMap.entrySet()){
             Long seatId = entry.getKey();
-            List<ShinhanPost> postList = entry.getValue();
+            List<MastercardPost> postList = entry.getValue();
 
             SeatDto.seatAverage seatAverage = new SeatDto.seatAverage();
-            ShinhanSeat seat = shinhanRepository.findById(seatId).orElse(null);
+            MastercardSeat seat = seatRepo.findById(seatId).orElse(null);
 
             if(seat != null){
                 seatAverage.setX(seat.getX());
@@ -167,25 +161,23 @@ public class ShinhanService {
             else{
                 seatAverage.setPostedYN(Boolean.TRUE);
                 int totalScore = 0;
-                for(ShinhanPost p : postList)
+                for(MastercardPost p : postList)
                     totalScore += (p.getViewScore() + p.getSoundScore() + p.getLightScore() + p.getSeatScore());
                 int average = totalScore / (postList.size() * 4);
                 seatAverage.setAverage(average);
             }
             seatAverages.add(seatAverage);
         }
-
         return seatAverages;
     }
-
     private User getUser(Long userId){
-        return userRepository.findById(userId).orElseThrow();
+        return userRepo.findById(userId).orElseThrow();
     }
     private Theater getTheater(Long theaterId){
-        return theaterRepository.findById(theaterId).orElseThrow();
+        return theaterRepo.findById(theaterId).orElseThrow();
     }
     private List<Image> getImage(List<Image> images){
-        List<Image> tmp = imageRepository.findAll();
+        List<Image> tmp = imageRepo.findAll();
         for (Image a : tmp){
             for(Image i : images){
                 if(a.getImgUrl().equals(i.getImgUrl()))
@@ -194,10 +186,10 @@ public class ShinhanService {
         }
         return images;
     }
-    private Optional<ShinhanSeat> getSeat(Long seatId){
-        return shinhanRepository.findById(seatId);
+    private MastercardSeat getSeat(Long seatId){
+        return seatRepo.findById(seatId).orElseThrow();
     }
-    private ShinhanPost getSeatPost(Long viewNo){
-        return shinhanPostRepository.findById(viewNo).get();
+    private MastercardPost getSeatPost(Long viewNo){
+        return postRepo.findByViewNo(viewNo);
     }
 }
