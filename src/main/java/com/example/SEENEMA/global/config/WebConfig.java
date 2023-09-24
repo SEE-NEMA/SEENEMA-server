3e7ec67cc1e569612d4aa46b1f35eb9d77c8af27
jihyunna <skwlgus106@naver.com>
2023-09-24 14:46:03 +0900

    :rocket: config 수정

--- src/main/java/com/example/SEENEMA/global/config/WebConfig.java
+++ src/main/java/com/example/SEENEMA/global/config/WebConfig.java
@@ -10,8 +10,8 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
-                .allowedOrigins("http://192.168.0.127:3000")
+                .allowedOrigins("http://localhost:3000",
+                "http://192.168.0.127:3000")
                 .allowedMethods("GET", "POST", "PUT", "DELETE");
     }
 }
